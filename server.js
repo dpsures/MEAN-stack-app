81cfbb52698a5fa6e1e1760b1755868c31e50a4b
Suresh Kumar Devaraj <dpsures@gmail.com>
2017-09-30 20:41:39 +0530

    mongodb included

--- server.js
+++ server.js
@@ -1,16 +1,34 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const path = require('path');
+const http = require('http');
 
-app.use(express.static(__dirname + '/dist'));
+// API file for interacting with MongoDB
+const api = require('./server/routes/api');
+
+// Parsers
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+
+app.use(express.static(path.join(__dirname + '/dist')));
+
+// API location
+app.use('/api', api);
+
+// angular use PathLocationStrategy
+app.get('*', function(req, res){
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
 
 app.set('port', (process.env.PORT || 5000));
 
-app.listen(app.get('port'), function() {
+const port = app.get('port');
+
+const server = http.createServer(app);
+
+server.listen(port, () => console.log('Server Running on port :', port));
+
+/*app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
-  });
-  
-// angular use PathLocationStrategy
-app.get('/*', function(req, res){
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
-});
+  });*/
